7460cb15864965ed019a31482c9dd2b943958391
kfgarrity <kevin.garrity@nist.gov>
2022-11-17 19:03:08 -0500

    quick update lappy

--- src/Galerkin.jl
+++ src/Galerkin.jl
@@ -3,12 +3,13 @@ using Polynomials
 using SpecialPolynomials
 using Base.Threads
 using LinearAlgebra
-
+using ForwardDiff
+using QuadGK
 
 function hydrogen(Z, N, PTS, W, Bvals,S, d2; a = 0.0, b = 20.0, M = -1)
 
     if M == -1
-        M = min(203, Int64(round(N * 1.5 + 3)))
+        M = min(200, Int64(round(N * 1.5 + 3)))
     end
 #    PINT = pre_do_integral(N, PTS, W, Bvals)    
 
@@ -53,7 +54,72 @@ function hydrogen(Z, N, PTS, W, Bvals,S, d2; a = 0.0, b = 20.0, M = -1)
 
 end
 
+#=function hydrogen_grid(Z, N, PTS, W, Bvals,S, d2; a = 0.0, b = 20.0, M = -1, α = 0.1)
+
+    if M == -1
+        M = min(203, Int64(round(N * 1.5 + 3)))
+    end
+#    PINT = pre_do_integral(N, PTS, W, Bvals)    
+
+  
+    function V(r)
+        return -Z / r
+    end
+
+    β = b / (exp(α * b)-1)
     
+    function r(x)
+        return β*(exp(α*x) - 1)
+    end
+
+    r1 = x -> ForwardDiff.Derivative(r, x)
+    r2 = x -> ForwardDiff.Derivative(r1, x)
+    r3 = x -> ForwardDiff.Derivative(r2, x)
+    
+    
+#=
+    function X(r)
+        return (r - L) / (r+L)
+    end
+    function R(x)
+        return L*(1 + x) / (1 - x)
+    end
+
+    function dR(x)
+        return L*(  1.0 / (1 - x) + (1+x)*(1-x)^2)
+    end
+    =#
+
+    function X(r)
+        return -1.0 + 2 * (a - r)/(a-b)
+    end
+    function R(x)
+        return a .+ (x .- -1.0)*(b-a) / 2.0
+    end
+    
+
+    function Vm(x)
+
+        return (3 * (r2(r(R(x))))^2 - 2 * r3(r(R(x))) * r1(r(R(x))) )  / (4 * r1(r(R(x)))^4)
+        
+    end
+    
+    
+    function Vx(x)
+        return (V(r(R(x))) + (3 * (r2(r(R(x))))^2 - 2 * r3(r(R(x))) * r1(r(R(x))) )  / (4 * r1(r(R(x)))^4) ) * r1(R(x))
+    end
+
+    
+    INT = do_integral(Vx, N, PTS, W, Bvals; M = M)
+
+
+    #   INT = do_integral(Vx,  PINT, PTS)
+    vals, vects = eigen(-0.5*(d2[1:N-1, 1:N-1] /  (b-a)^2*2^2  ) + INT , (S[1:N-1, 1:N-1]))
+
+    return vals
+
+end
+=#    
 
 function do_integral(fn, N, PTS, W, Bvals; M = 199)
 
@@ -281,6 +347,7 @@ function D2(B)
 end
 
 
+
 function f(V, B)
 
     d = zeros(length(B), length(B))
@@ -294,6 +361,68 @@ function f(V, B)
 
 end
 
+
+function get_cheb_bc_grid(N, r)
+
+    B = basis.(Chebyshev{Float64}, 0:N);
+
+    B2 = []
+    for n = 3:N+1
+        if mod(n,2) == 0
+            push!(B2, B[n] - B[2])
+        else
+            push!(B2, B[n] - B[1])
+        end
+    end
+
+    B3 = []
+    for b in B2
+        #push!(B3, b / sqrt(integrate(b^2, -1, 1)))
+
+        push!(B3, b / sqrt(QuadGK.quadgk(x->b(r(x))^2, -1,1)[1]))
+
+    end
+    return B3
+    
+end
+
+function stuff(N, α)
+    #C = basis.(Chebyshev{Float64}, 0:N)
+
+    b = 1.0
+    a = -1.0
+    β = (b-a) / ((exp(α * (b-a))- 1 ) )
+    function r(x)
+        return β*(exp(α*(x-a)) - 1 )  + a
+    end
+    
+    
+    B = get_cheb_bc_grid(N, r)
+    S = zeros(length(B), length(B))
+    for (c1, b1) in enumerate(B)
+        for (c2, b2) in enumerate(B)
+            S[c1,c2] = QuadGK.quadgk(x->b1(r(x))*b2(r(x)), -1,1)[1]
+            #p = Polynomial(b1*b2)
+            #S[c1,c2] = QuadGK.quadgk(p, -1,1)[1]
+        end
+    end
+    
+    d2 = zeros(length(B), length(B))
+    for (c1, b1) in enumerate(B)
+        b1_1 = bb -> ForwardDiff.derivative(aa->b1(r(aa)), bb)
+        b1_2 = cc -> ForwardDiff.derivative(b1_1, cc)
+        for (c2, b2) in enumerate(B)
+            d2[c1,c2] = QuadGK.quadgk(x->b1_2(x)*b2(r(x)), -1,1)[1]
+            #p = Polynomial(b1*b2)
+            #S[c1,c2] = QuadGK.quadgk(p, -1,1)[1]
+        end
+    end
+    
+    
+            
+
+    return S, d2
+end
 
 
 
